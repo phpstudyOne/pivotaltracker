e679c71a89d1bb2104b209940a24dfc37b514b1a
Jason Zhang <845830229@qq.com>
2018-03-21 05:10:55 -0500

    init

---
+++ js/common_end.js
@@ -0,0 +1,6 @@
+(()=>{
+    setInterval(function() {
+        $('a[data-cid=c925]').css({"font-size":"20px","color":'#ec3909'});
+        $('a[data-cid=c928]').css({"font-size":"20px","color":'#FF66FF'});
+      }, 3000)
+})()
