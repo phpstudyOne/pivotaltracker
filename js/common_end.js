4e5687b4c7d6c5bcf1ce85e8cc5ddd36b3a74b3b
Jason Zhang <jaszhang@xogrp.com>
2018-03-23 11:19:33 +0800

    添加配置页

--- js/common_end.js
+++ js/common_end.js
@@ -1,6 +1,13 @@
 (()=>{
-    setInterval(function() {
-        $('a[data-cid=c925]').css({"font-size":"20px","color":'#ec3909'});
-        $('a[data-cid=c928]').css({"font-size":"20px","color":'#FF66FF'});
-      }, 3000)
+      setTimeout(aaa,5000);
+
+      $('body').click(function(){
+        aaa();
+      });
+
+      function aaa(){
+        $('a:contains(v1.19.1)').css({"font-size":"20px","color":'#ec3909'});
+        $('a:contains(v1.19.2)').css({"font-size":"20px","color":'#FF66FF'});
+      }
+
 })()
