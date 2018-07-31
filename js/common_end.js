fb62497be345eb8e3dc710099ab01ae9011b0430
Jason Zhang <jaszhang@xogrp.com>
2018-07-31 19:40:51 +0800

    remove lableName styler

--- js/common_end.js
+++ js/common_end.js
@@ -9,7 +9,7 @@
       function hanld(){
         chrome.storage.local.get((data)=>{
           data && Object.keys(data.labelName).forEach((labelName)=>{
-            $(`a:contains(${labelName})`) && $(`a:contains(${labelName})`).css({"font-size":"20px","color":`'${data.labelName[labelName]}'`});
+            $(`a:contains(${labelName})`) && $(`a:contains(${labelName})`).css({"font-size":"20px","color":`${data.labelName[labelName]}`});
           }); 
         });
       }
